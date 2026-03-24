4464aa91208f74ea197e6601e8d2eec3d38fb256
Artyom 0_0 <artem.rukavichnikov.03@mail.ru>
2026-03-24 16:40:37 +0500

    Filmorate project

---
+++ src/test/java/UserControllerTest.java
@@ -0,0 +1,195 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.controller.UserController;
+import ru.yandex.practicum.filmorate.exception.ConditionsNotMetException;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    private UserController userController;
+
+    @BeforeEach
+    void setUp() {
+        userController = new UserController();
+    }
+
+    @Test
+    void addUser_validUser_shouldAddSuccessfully() {
+        User user = User.builder()
+                .email("test@mail.com")
+                .login("testuser")
+                .name("Test User")
+                .birthday(LocalDate.of(2000, 1, 1))
+                .build();
+
+        User added = userController.addUser(user);
+
+        assertNotNull(added.getId());
+        assertEquals("test@mail.com", added.getEmail());
+        assertEquals(1, userController.getAllUsers().size());
+    }
+
+    @Test
+    void addUser_emptyEmail_shouldThrowException() {
+        User user = User.builder()
+                .email("")
+                .login("login")
+                .name("Name")
+                .birthday(LocalDate.of(2000, 1, 1))
+                .build();
+
+        assertThrows(ConditionsNotMetException.class, () -> userController.addUser(user));
+    }
+
+    @Test
+    void addUser_isBlankEmail_shouldThrowException() {
+        User user = User.builder()
+                .email(" user@mail.com   ")
+                .login("login")
+                .name("Name")
+                .birthday(LocalDate.of(2000, 1, 1))
+                .build();
+
+        assertThrows(ConditionsNotMetException.class, () -> userController.addUser(user));
+    }
+
+    @Test
+    void addUser_emptyLogin_shouldThrowException() {
+        User user = User.builder()
+                .email("user@mail.com")
+                .login("")
+                .name("Name")
+                .birthday(LocalDate.of(2000, 1, 1))
+                .build();
+
+        assertThrows(ConditionsNotMetException.class, () -> userController.addUser(user));
+    }
+
+    @Test
+    void updateUser_existingUser_shouldUpdateSuccessfully() {
+        User user = User.builder()
+                .email("user@mail.com")
+                .login("user1")
+                .name("User One")
+                .birthday(LocalDate.of(1995, 5, 5))
+                .build();
+
+        User added = userController.addUser(user);
+
+        User update = User.builder()
+                .id(added.getId())
+                .email("new@mail.com")
+                .login("user101")
+                .name("User Updated")
+                .birthday(LocalDate.of(1995, 5, 5))
+                .build();
+
+        User updated = userController.updateUser(update);
+
+        assertEquals("new@mail.com", updated.getEmail());
+        assertEquals("User Updated", updated.getName());
+    }
+
+    @Test
+    void updateUser_nonExistingId_shouldThrowNotFound() {
+        User update = User.builder()
+                .id(999L)
+                .email("new@mail.com")
+                .login("user1")
+                .name("User Updated")
+                .birthday(LocalDate.of(1995, 5, 5))
+                .build();
+
+        assertThrows(NotFoundException.class, () -> userController.updateUser(update));
+    }
+
+    @Test
+    void updateUser_nonExistingBirthday_shouldThrowException() {
+        User user = User.builder()
+                .email("user@mail.com")
+                .login("user1")
+                .name("User One")
+                .birthday(LocalDate.of(1995, 5, 5))
+                .build();
+
+        User added = userController.addUser(user);
+
+        User update = User.builder()
+                .id(added.getId())
+                .email("new@mail.com")
+                .login("user101")
+                .name("User Updated")
+                .birthday(LocalDate.of(2030, 5, 5))
+                .build();
+
+        assertThrows(ConditionsNotMetException.class, () -> userController.updateUser(update));
+    }
+
+    @Test
+    void addUser_duplicateName_shouldThrowException() {
+        User user1 = User.builder()
+                .email("user1@mail.com")
+                .login("login1")
+                .name("John Doe")
+                .birthday(LocalDate.of(1995, 5, 5))
+                .build();
+
+        userController.addUser(user1);
+
+        User user2 = User.builder()
+                .email("user2@mail.com")
+                .login("login2")
+                .name("John Doe")
+                .birthday(LocalDate.of(1995, 5, 5))
+                .build();
+
+        assertThrows(ConditionsNotMetException.class, () -> userController.addUser(user2));
+    }
+
+    @Test
+    void addUser_duplicateLogin_shouldThrowException() {
+        User user1 = User.builder()
+                .email("user1@mail.com")
+                .login("login123")
+                .name("User One")
+                .birthday(LocalDate.of(1995, 5, 5))
+                .build();
+
+        userController.addUser(user1);
+
+        User user2 = User.builder()
+                .email("user2@mail.com")
+                .login("login123")
+                .name("User Two")
+                .birthday(LocalDate.of(1995, 5, 5))
+                .build();
+
+        assertThrows(ConditionsNotMetException.class, () -> userController.addUser(user2));
+    }
+
+    @Test
+    void addUser_duplicateEmail_shouldThrowException() {
+        User user1 = User.builder()
+                .email("duplicate@mail.com")
+                .login("login1")
+                .name("User One")
+                .birthday(LocalDate.of(1995, 5, 5))
+                .build();
+
+        userController.addUser(user1);
+
+        User user2 = User.builder()
+                .email("duplicate@mail.com")
+                .login("login2")
+                .name("User Two")
+                .birthday(LocalDate.of(1995, 5, 5))
+                .build();
+
+        assertThrows(ConditionsNotMetException.class, () -> userController.addUser(user2));
+    }
+}
